db8e9b9212a5cca8b4c85701664d1f9544c0f1d8
Oksana-Pak <okspak7@gmail.com>
2022-10-24 11:35:57 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+function setTime({ duration, seconds }) {
+  if (seconds === duration) {
+    localStorage.removeItem(STORAGE_KEY);
+  } else {
+    localStorage.setItem(STORAGE_KEY, seconds);
+  }
+}
