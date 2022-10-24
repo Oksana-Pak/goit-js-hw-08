5953bc474f139817e3b609e27b746892cc3a5c9b
Oksana-Pak <okspak7@gmail.com>
2022-10-24 11:52:02 +0300

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = { email: '', message: '' };
+populateForm();
+
+function onFormInput(e) {
+  const savedLocalStorage = getItemLocalStorage();
+  if (savedLocalStorage) {
+    if (savedLocalStorage.email) {
+      formData.email = savedLocalStorage.email;
+    }
+    if (savedLocalStorage.message) {
+      formData.message = savedLocalStorage.message;
+    }
+  }
+
+  formData[e.target.name] = e.target.value;
+
+  setItemLocalStorage(formData);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(getItemLocalStorage());
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateForm() {
+  const savedData = getItemLocalStorage();
+  if (!savedData) {
+    return;
+  }
+  formRef[0].value = savedData.email ? savedData.email : '';
+  formRef[1].value = savedData.message ? savedData.message : '';
+}
+
+function setItemLocalStorage(obj) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(obj));
+}
+
+function getItemLocalStorage() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY));
+}
